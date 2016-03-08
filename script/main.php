f431b3ae604d14a77fbabc1937874b8db762ad23
jcrjaci <jacinto_rodrigues1@hotmail.com>
2016-03-08 22:43:37 +0000

    add messages

--- script/main.php
+++ script/main.php
@@ -1,27 +1,37 @@
 <?php
+
 //LINK OF JENKINS API
-$link ='http://localhost:8080/api/json?tree=jobs[displayName,lastBuild[result]]';
+$link = 'http://localhost:8080/api/json?tree=jobs[displayName,lastBuild[result]]';
 
+try {
 //GET RESPONSE OF JENKINS API
-$jobsString = file_get_contents($link);
-
+    $jobsString = file_get_contents($link);
 //CONVERT STRING TO JSON
-$jobs = json_decode($jobsString);
+    $jobs = json_decode($jobsString);
 //NEW CONNECTION TO DATABASE
-$db = new SQLite3('Jenkins');
+    $db = new SQLite3('Jenkins');
 //HEAD OF INSERT QUERIE
-$insert = 'INSERT INTO jobs (name, status) VALUES ';
+    $insert = 'INSERT INTO jobs (name, status) VALUES ';
 //LOOP THAT FULLFILLS THE INSERT QUERY
-foreach ($jobs->jobs as $key => $value) {
-    
-    $insert .="("
-            . "'".$value->displayName."',"
-            . "'".$value->lastBuild->result."'"
-            . "),";
-}
+    foreach ($jobs->jobs as $key => $value) {
+
+        $insert .="("
+                . "'" . $value->displayName . "',"
+                . "'" . $value->lastBuild->result . "'"
+                . "),";
+    }
 //DELETE THE LAST COMMA IN THE QUERIE
-$insert = rtrim($insert,",");
+    $insert = rtrim($insert, ",");
 //EXECUTE THE QUERY
-$db->exec($insert);
+    $saved =$db->exec($insert);
+    
+    if ($saved) {
+        echo "Jobs successfully inserted in database";
+    }else{
+        echo "An error occur, and  weren't inserted any job";
+    }
 //CLOSE CONNECTION
-$db->close();
+    $db->close();
+} catch (Exception $e) {
+    echo "Error:" . $e->getMessage();
+}
