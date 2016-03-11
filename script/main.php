8273b0b68b89176c87f24a57bf2799683d65419d
jcrjaci <jacinto_rodrigues1@hotmail.com>
2016-03-11 12:36:53 +0000

    Update main.php

--- script/main.php
+++ script/main.php
@@ -1,4 +1,5 @@
 <?php
+
 //LINK OF JENKINS API
 $link = 'http://localhost:8080/api/json?tree=jobs[displayName,lastBuild[result]]';
 
@@ -8,29 +9,28 @@ try {
 //CONVERT STRING TO JSON
     $jobs = json_decode($jobsString);
 //NEW CONNECTION TO DATABASE
-	$db = new PDO('sqlite:Jenkins');
+    $db = new PDO('sqlite:Jenkins');
 //QUERIE
     $query = 'INSERT INTO jobs (name, status) VALUES (:name, :status)';
 //INSERT JOBS IN DATABASE 
     if (sizeof($jobs->jobs) > 0) {
-		
+
         foreach ($jobs->jobs as $key => $value) {
 
             $sql = $db->prepare($query);
             $sql->bindParam(':name', $value->displayName, PDO::PARAM_STR);
             $sql->bindParam(':status', $value->lastBuild->result, PDO::PARAM_STR);
-			$sql->execute();
-			
-			$errors = $sql->errorInfo();
+            $sql->execute();
+
+            $errors = $sql->errorInfo();
 //IF AN ERROR OCCUR DISPLAY THE ERROR
-			if($errors[0]!=="00000"){
-				echo "For Job ".$value->displayName ." ERROR:".$errors[2];
-			}		
+            if ($errors[0] !== "00000") {
+                echo "For Job " . $value->displayName . " ERROR:" . $errors[2];
+            }
         }
-		
-		echo "DONE!!!!";
-		
-     } else {
+
+        echo "DONE!!!!";
+    } else {
         echo "There isn't any job to insert";
     }
 } catch (Exception $e) {
